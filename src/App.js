a796033350c7ab525a6d09de602e50739e74cb16
sanaya <sanayasaeed68@gmail.com>
2022-03-25 11:25:59 +0530

    added variable colours

--- src/App.js
+++ src/App.js
@@ -8,12 +8,15 @@ import Profile from './pages/Profile/Profile';
 import MarketPlace from './pages/MarketPlace/MarketPlace';
 
 import ProfileDashboard from './pages/profileDashboard/ProfileDashboard';
+import CoachProfile from './pages/CoachProfile/CoachProfile';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/profile" element={<Profile />}></Route>
+        <Route exact path="/coachprofile" element={<CoachProfile />}></Route>
+
         <Route exact path="/market-place" element={<MarketPlace />}></Route>
         <Route exact path="/" element={<Login />} />
         <Route exact path="/dashboard" element={<ProfileDashboard />} />
