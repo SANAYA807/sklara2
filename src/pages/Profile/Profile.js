2e43129212115190292f660291325e9525c9481a
Gaurav_Singh <gauravkarki038@gmail.com>
2022-04-05 15:28:06 +0530

    profileData

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -3,8 +3,36 @@ import Navbar from '../../components/navbar/Navbar';
 import SettingsApplicationsIcon from '@mui/icons-material/SettingsApplications';
 import './Profile.css';
 import ContactFooter from '../../components/ContactFooter/ContactFooter';
+import { user } from '../../components/auth/authhelper';
+import { useEffect } from 'react';
+import formatDistance from 'date-fns/formatDistance'
+
 
 function Profile({userdata}) {
+  console.log(userdata)
+
+  const LanguageArr = userdata.communicationLanguages;
+
+const formatDate = (dateString) => {
+  const options = { year: "numeric", month: "long", day: "numeric" }
+  return new Date(dateString).toLocaleDateString(undefined, options)
+}
+
+const firstLogin = formatDate(userdata.firstLogin)
+const lastLogin = userdata.lastLogin && formatDate(userdata.lastLogin)
+  
+
+function getTimeFormat(dateStr) {
+
+  const str = formatDistance(
+      new Date(dateStr),
+      new Date()
+  );
+  return <>{str}</>
+  
+}
+
+
   return (
     <>
     <Navbar userdata={userdata} />
@@ -16,19 +44,19 @@ function Profile({userdata}) {
           <div className='col-lg-6 col-sm-12 common-card'>
             <div className="card">
               <button type="button" className="btn profile-settings"><i class="fa-solid fa-gear"></i></button>
-              <img src="images/dummy/user4_big.jpg" className="card-img-top profile-image" alt="..." />
+              <img src={userdata.profilePicture} className="card-img-top profile-image" alt="..." />
               <div className="card-body">
                 <span className='profile-flag1'><img className="" src="	http://18.157.84.45/design/images/flags/de.svg" alt="" /></span>
                 <span className="profile-flag2"><img className="img-fluid" src="	http://18.157.84.45/design/images/flags/en.svg" alt="" /></span>
-                <h4 className="card-title mb-5">Rebecca Finch</h4>
-                <p className='mb-5' style={{ "color": "#cccccc" }}><small>Senior Project Manager</small></p>
+                <h4 className="card-title mb-5">{userdata.firstName} {userdata.lastName}</h4>
+                <p className='mb-5' style={{ "color": "#cccccc" }}><small>{userdata.UserRoleType}</small></p>
                 <hr className='text-muted' />
                 <div className='d-flex justify-content-between'>
                   <small className='text-muted'>Platform Language</small>
                   <small className='text-muted'>Default Platform Mode</small>
                 </div>
                 <div className='d-flex justify-content-between'>
-                  <p>English</p>
+                  <p>{userdata.dashboardLanguage}</p>
                   <p>Training</p>
 
                   </div>
@@ -41,46 +69,48 @@ function Profile({userdata}) {
                   <div className='col-6'>
                     <div className="mb-4">
                       <small className='text-muted m-0 p-0'>From</small>
-                      <p className='m-0 p-0' style={{ "fontWeight": "500" }}>Germany</p>
+                      <p className='m-0 p-0' style={{ "fontWeight": "500" }}>{userdata.currentLocation}</p>
                     </div>
                     <div className="mb-4">
                       <small className='text-muted m-0 p-0'>Works At</small>
-                      <p className='m-0 p-0' style={{ "fontWeight": "500" }}>Bosch Ingeneurgeleschaft Gmbh</p>
+                      <p className='m-0 p-0' style={{ "fontWeight": "500" }}>{userdata.companyName}</p>
                     </div>
                     <div className="mb-4">
                       <small className='text-muted m-0 p-0'>Email Address</small>
-                      <p className='m-0 p-0' style={{ "fontWeight": "500" }}>rebecca@somemail.com</p>
+                      <p className='m-0 p-0' style={{ "fontWeight": "500" }}>{userdata.email}</p>
                     </div>
                     <div className="mb-4">
                       <small className='text-muted m-0 p-0'>Date of Birth</small>
-                      <p className='m-0 p-0' style={{ "fontWeight": "500" }}>16.07.1972</p>
+                      <p className='m-0 p-0' style={{ "fontWeight": "500" }}>{userdata.dob}</p>
                     </div>
                     <div className="mb-4">
-                      <small className='text-muted m-0 p-0'>Experience</small>
-                      <p className='m-0 p-0' style={{ "fontWeight": "500" }}>21 years</p>
+                      <small className='text-muted m-0 p-0'>Gender</small>
+                      <p className='m-0 p-0' style={{ "fontWeight": "500" }}>{userdata.gender}</p>
                     </div>
                   </div>
                   <div className='col-6'>
                     <div className="mb-4">
                       <small className='text-muted m-0 p-0'>Experience</small>
-                      <p className='m-0 p-0' style={{ "fontWeight": "500" }}>13 Years</p>
+                      <p className='m-0 p-0' style={{ "fontWeight": "500" }}>{userdata.experience} Years</p>
                     </div>
                     <div className="mb-4">
                       <small className='text-muted m-0 p-0'>Languages</small>
-                      <p className='m-0 p-0' style={{ "fontWeight": "500" }}>English, German</p>
+                      <p className='m-0 p-0' style={{ "fontWeight": "500" }}>{LanguageArr.map((item, index)=> { return (
+                        <> {index!=0 ? `,${item}` : item  } </> 
+                        ) }) }</p>
                     </div>
                     <div className="mb-4">
                       <small className='text-muted m-0 p-0'>Phone Number</small>
-                      <p className='m-0 p-0' style={{ "fontWeight": "500" }}>+49 172 575 9260</p>
+                      <p className='m-0 p-0' style={{ "fontWeight": "500" }}>{userdata.phoneNumber}</p>
                     </div>
                     <div className="mb-4">
                       <small className='text-muted m-0 p-0'>Highest Qualification</small>
-                      <p className='m-0 p-0' style={{ "fontWeight": "500" }}>Dr. Ing</p>
+                      <p className='m-0 p-0' style={{ "fontWeight": "500" }}>{userdata.highestEducation}</p>
                     </div>
                     <div className="mb-4">
                       <small className='text-muted m-0 p-0'>Linked In Profile</small>
                       <div>
-                        <a href='/' className='m-0 p-0' style={{ "fontWeight": "500", "textDecoration": "none" }}>Open Profile</a>
+                        <a href={userdata.linkedIn} className='m-0 p-0' style={{ "fontWeight": "500", "textDecoration": "none" }}>Open Profile</a>
                       </div>
                     </div>
                   </div>
@@ -153,13 +183,13 @@ function Profile({userdata}) {
                   <div className='card mb-5' style={{ "boxShadow": "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px", "backgroundColor": "#fff3cd" }}>
                     <div className='card-body pb-1'>
                       <p style={{ "fontWeight": "500" }}><small>First access to site</small></p>
-                      <p><small>Monday, 20th November 2021, 4:43 PM (96 days ago)</small></p>
+                      <p><small>{firstLogin}, {getTimeFormat(userdata.firstLogin)}</small></p>
                     </div>
                   </div>
                   <div className='card' style={{ "boxShadow": "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px", "backgroundColor": "#d4edda" }}>
                     <div className='card-body pb-1'>
                       <p style={{ "fontWeight": "500" }}><small>Last access to site</small></p>
-                      <p><small>Friday, 11th February 2021, 10:17 AM (2 days ago)</small></p>
+                      <p><small>{lastLogin}, {userdata.lastLogin && getTimeFormat(userdata.lastLogin)}</small></p>
                     </div>
                   </div>
                 </div>
